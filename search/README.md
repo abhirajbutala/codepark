68e95845fc945e103b51b7e5e73842f9d3fdbe2c
abhirajbutala <abhiraj.butala@gmail.com>
2014-08-16 01:47:15 -0700

    Implementation for counting the number of occurances of an element in a sorted array.

--- search/README.md
+++ search/README.md
@@ -1,5 +1,7 @@
 Search Problems
 ===============
 
-* Find a minimum element in a sorted array.
+* Binary Search (Non-Recursive).
+* Binary Search (Recursive).
 * Find a minimum element in a sorted array which is also rotated.
+* Count the number of occurances of an element in a sorted array.
--- search/binSearch.cc
+++ search/binSearch.cc
@@ -79,11 +79,62 @@ void rotatedArrayElemSearch(int *arr, int size, int elem) {
 
 
 /*---------------------------------------------------------------------------*/
+/* Please refer to Algorithm Design Manual Section 4.9.1 (Page 133) for more
+ * details on the algorithm.
+ *
+ * This algorithm is O(lg n), which is better than O(lg n + s) algorithm in
+ * which the binary search for the element is performed and then the low and
+ * high positions of the element are found after linear traversal from the
+ * position returned by binary search.
+ */
+int getLowPos(int *arr, int low, int high, int elem) {
+    if (low > high) {
+        return low;
+    }
+
+    int mid = (low + high)/2;
+    if (arr[mid] < elem) {
+        return getLowPos(arr, mid+1, high, elem);
+    } else {
+        return getLowPos(arr, low, mid-1, elem);
+    }
+}
+
+int getHighPos(int *arr, int low, int high, int elem) {
+    if (high < low) {
+        return high;
+    }
+
+    int mid = (low + high)/2;
+    if (arr[mid] > elem) {
+        return getHighPos(arr, low, mid-1, elem);
+    } else {
+        return getHighPos(arr, mid+1, high, elem);
+    }
+}
+
+int elementCount(int *arr, int low, int high, int elem) {
+    if (arr == NULL) {
+        return 0;
+    }
+
+    // Find low and high positions of the element & return the difference.
+    int lowPos = getLowPos(arr, low, high, elem);
+    //cout << "low position is : " << lowPos << endl;
+    int highPos = getHighPos(arr, low, high, elem);
+    //cout << "high position is : " << highPos << endl;
+
+    return (highPos - lowPos + 1);
+}
+
+
+/*---------------------------------------------------------------------------*/
 int main(int argc, char *argv[]) {
 
     int pos;
     int arr[] = {1, 3, 5, 6, 9, 10, 11};
 
+    /* Binary Search Non-Recursive */
     pos = binSearch(arr, 0, 6, 1);
     assert(pos == 0);
     pos = binSearch(arr, 0, 6, 5);
@@ -94,6 +145,7 @@ int main(int argc, char *argv[]) {
     assert(pos == -1);
 
 
+    /* Binary Search Recursive */
     pos = binSearchRecursive(arr, 0, 6, 6);
     assert(pos == 3);
     pos = binSearchRecursive(arr, 0, 6, 11);
@@ -104,17 +156,23 @@ int main(int argc, char *argv[]) {
     assert(pos == -1);
 
 
-
+    /* Minimum element from Sorted Rotated Array */
     int arr2[] = {5, 6, 7, 8, 9, 10, 20, 40};
     rotatedArrayMinSearch(arr2, 8);
-
     int arr3[] = {5, 6, 7, 8, 9, 9, 1, 2, 3};
     rotatedArrayMinSearch(arr3, 9);
-
     int arr4[] = {5};
     rotatedArrayMinSearch(arr4, 1);
-
     int arr5[] = {5, 1, 2, 3};
     rotatedArrayMinSearch(arr5, 4);
+
+    /* Counting occurances of a element in sorted array */
+    int arr6[] = {1, 2, 3, 4, 4, 4, 4, 5, 5, 6};
+    assert (elementCount(arr6, 0, 9, 2) == 1);
+    assert (elementCount(arr6, 0, 9, 4) == 4);
+    assert (elementCount(arr6, 0, 9, 5) == 2);
+    assert (elementCount(arr6, 0, 9, 10) == 0);
+    assert (elementCount(arr6, 0, 9, -1) == 0);
+
     return 0;
 }
